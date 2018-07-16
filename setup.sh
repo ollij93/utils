620d6d786722a17d32c6ef4cb9ebc259b9f55429
Olli Johnson <ollijohnson93+github@gmail.com>
2018-07-16 12:05:37 +0100

    Remove the todoist stuff and update the setup.sh script

--- setup.sh
+++ setup.sh
@@ -6,13 +6,6 @@ set -e
 # Set the UTILS_DIR for use here and globally
 export UTILS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-# Create the todoist virtual environment and install the todoist python module
-if ! [ -e ${UTILS_DIR}/todoist-env ] ; then
-    virtualenv -p /usr/bin/python2.7 ${UTILS_DIR}/todoist-env
-    source ${UTILS_DIR}/todoist-env/bin/activate
-    pip install -e ${UTILS_DIR}/todoist-python/
-fi
-
 # Link the tmux config
 [ -e ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.old
 ln -s ${UTILS_DIR}/.tmux.conf ~/.tmux.conf
@@ -21,12 +14,7 @@ ln -s ${UTILS_DIR}/.tmux.conf ~/.tmux.conf
 [ -e ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.old
 ln -s ${UTILS_DIR}/.vimrc ~/.vimrc
 
-# Append to .bashrc
-if [ ! -e ~/.bashrc ]; then
-    touch ~/.bashrc
-fi
-if [[ "`tail -n1 ~/.bashrc`" != "# !!! SETUP.SH END" ]]; then
-    echo "export UTILS_DIR=${UTILS_DIR}" >> ~/.bashrc
-    cat ${UTILS_DIR}/.bashrc >> ~/.bashrc
-    echo "# !!! SETUP.SH END" >> ~/.bashrc
-fi
+# Provide info to append to .bashrc
+echo "Add the following to your .bashrc to import the utils .bashrc:"
+echo "export UTILS_DIR=${UTILS_DIR}"
+echo "source ${UTILS_DIR}/.bashrc"
