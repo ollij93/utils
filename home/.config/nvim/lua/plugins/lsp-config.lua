3b9d2eb03a51f706b166085cb60d043489450ab2
Oliver Johnson <olijohns@cisco.com>
2026-08-18 07:48:35 -0700

    Add the basedpyright lsp for python code

--- home/.config/nvim/lua/plugins/lsp-config.lua
+++ home/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,24 +1,29 @@
+local lsps = {"lua_ls", "basedpyright"};
+
 return {
     {
         "mason-org/mason-lspconfig.nvim",
         lazy=false,
         opts = {
-            ensure_installed = { "lua_ls" },
+            ensure_installed = lsps
         },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
             {
                 "neovim/nvim-lspconfig",
                 config = function()
-                    -- local lspconfig = require("lspconfig")
-                    vim.lsp.config("lua_ls", {})
+                    for _, lsp in ipairs(lsps) do
+                        vim.lsp.config(lsp, {})
+                    end
+                end,
+                keys = {
+                    {"K", vim.lsp.buf.hover, {}},
+                    {"<leader>gd", vim.lsp.buf.definition, {}},
+                    {"<leader>gr", vim.lsp.buf.references, {}},
+                    {"<leader>ca", vim.lsp.buf.code_action, {}}
+                }
 
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-                    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-                    vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-                end
-            }
+            },
         },
     },
 }
