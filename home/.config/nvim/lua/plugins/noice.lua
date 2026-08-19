5fdd5a77d892accd9b934424b4a431d7441a361e
Oliver Johnson <olijohns@cisco.com>
2026-08-19 04:20:46 -0700

    Nvim shell command output in noice popup

--- home/.config/nvim/lua/plugins/noice.lua
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -1,7 +1,22 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+        -- Route the shell command output to display in a noice view
+        routes = {
+            {
+                view = "popup",
+                filter = {
+                    event = "msg_show",
+                    kind = {
+                        "shell_out",
+                        "shell_err",
+                        "shell_ret",
+                    },
+                },
+            },
+        },
+    },
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"rcarriga/nvim-notify",
