8cdba48bcda38d94a08bb6bf43629adf8911a784
Oliver Johnson <olijohns@cisco.com>
2026-08-19 04:44:08 -0700

    Nvim completions with blink.cmp

--- home/.config/nvim/lua/plugins/lsp-config.lua
+++ home/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,45 +1,45 @@
 -- Extra helpers for using venv's for python formatting and lsps
 local function uv_wrap_tool(tool)
-    return {
-        inherit = true,
-        command = function()
-            return vim.fn.exepath("uv")
-        end,
-        prepend_args = function(_, context)
-            local root = vim.fs.root(context.buf, {
-                "uv.lock",
-                "pyproject.toml",
-            })
-            return {
-                "run",
-                "--no-sync",
-                "--project",
-                root,
-                "--",
-                tool,
-            }
-        end
-    }
+	return {
+		inherit = true,
+		command = function()
+			return vim.fn.exepath("uv")
+		end,
+		prepend_args = function(_, context)
+			local root = vim.fs.root(context.buf, {
+				"uv.lock",
+				"pyproject.toml",
+			})
+			return {
+				"run",
+				"--no-sync",
+				"--project",
+				root,
+				"--",
+				tool,
+			}
+		end,
+	}
 end
 
 local function uv_wrap_lsp(tool, arguments)
-    return function(dispatchers, config)
-        local command = {
-            vim.fn.exepath("uv"),
-            "run",
-            "--no-sync",
-            "--project",
-            config.root_dir,
-            "--",
-            tool,
-        }
+	return function(dispatchers, config)
+		local command = {
+			vim.fn.exepath("uv"),
+			"run",
+			"--no-sync",
+			"--project",
+			config.root_dir,
+			"--",
+			tool,
+		}
 
-        vim.list_extend(command, arguments)
+		vim.list_extend(command, arguments)
 
-        return vim.lsp.rpc.start(command, dispatchers, {
-            cwd = config.root_dir,
-        })
-    end
+		return vim.lsp.rpc.start(command, dispatchers, {
+			cwd = config.root_dir,
+		})
+	end
 end
 
 -- Configure the LSPs and formatters for each language here.
@@ -59,11 +59,11 @@ local languages = {
 	python = {
 		lsps = {
 			ruff = {
-                cmd = uv_wrap_lsp("ruff", { "server" }),
-            },
+				cmd = uv_wrap_lsp("ruff", { "server" }),
+			},
 			ty = {
-                cmd = uv_wrap_lsp("ty", { "server" }),
-            },
+				cmd = uv_wrap_lsp("ty", { "server" }),
+			},
 		},
 		formatters = { "isort", "black" },
 	},
@@ -100,8 +100,10 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
 		config = function()
 			for lsp, opts in pairs(lsps) do
+				opts.capabilities = require("blink.cmp").get_lsp_capabilities()
 				vim.lsp.config(lsp, opts)
 			end
 		end,
@@ -112,7 +114,7 @@ return {
 			{ "<leader>ca", vim.lsp.buf.code_action, desc = "Code actions" },
 		},
 	},
-    -- Formatting configuration with conform.nvim
+	-- Formatting configuration with conform.nvim
 	{
 		"stevearc/conform.nvim",
 		dependencies = { "mason-org/mason.nvim" },
@@ -128,13 +130,40 @@ return {
 		},
 		opts = {
 			formatters_by_ft = formatters_by_ft,
-            default_format_opts = {
-                timeout_ms = 2000,
-            },
-            formatters = {
-                black = uv_wrap_tool("black"),
-                isort = uv_wrap_tool("isort"),
-            }
+			default_format_opts = {
+				timeout_ms = 2000,
+			},
+			formatters = {
+				black = uv_wrap_tool("black"),
+				isort = uv_wrap_tool("isort"),
+			},
+		},
+	},
+	-- Code completion with blink.cmp
+	{
+		"saghen/blink.cmp",
+		dependencies = { "saghen/blink.lib" },
+		build = function()
+			require("blink.cmp").build():pwait()
+		end,
+		opts = {
+			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+			-- 'super-tab' for mappings similar to vscode (tab to accept)
+			-- 'enter' for enter to accept
+			-- 'none' for no mappings
+			--
+			-- All presets have the following mappings:
+			-- C-space: Open menu or open docs if already open
+			-- C-n/C-p or Up/Down: Select next/previous item
+			-- C-e: Hide menu
+			-- C-k: Toggle signature help (if signature.enabled = true)
+			--
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			keymap = { preset = "super-tab" },
+
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
 		},
 	},
 }
